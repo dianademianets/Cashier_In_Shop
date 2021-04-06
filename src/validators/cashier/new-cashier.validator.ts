32581164bf3f25739b20e841f760d0895f1b25b4
Diana Demianets <66914510+dianademianets@users.noreply.github.com>
2021-04-06 18:39:45 +0200

    Update new-cashier.validator.ts

--- src/validators/cashier/new-cashier.validator.ts
+++ src/validators/cashier/new-cashier.validator.ts
@@ -3,7 +3,7 @@ import * as Joi from 'joi';
 import { RegExpEnum } from '../../constants';
 
 export const newCashierValidator = Joi.object({
-  experience: Joi.number().integer().min(1).max(100)
+  experience: Joi.number().integer().min(0).max(100)
     .required(),
   email: Joi.string().trim().regex(RegExpEnum.email).required(),
   days_work: Joi.string().trim().allow('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday').required(),
